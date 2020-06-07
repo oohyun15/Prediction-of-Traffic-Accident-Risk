1dddd8700e268e2102884f69b73ea88dbf1486bc
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-06-07 22:06:41 +0900

    remove pipeline

--- src/winprocess.py
+++ src/winprocess.py
@@ -29,10 +29,10 @@ preprocessing = preprocessing.iloc[np.random.permutation(len(preprocessing))]
 y = preprocessing.loc[:, 'TargetOrigin' ]
 x = preprocessing.loc[:,'Location_East':]
 
-from sklearn.model_selection import train_test_split
-
 x = x[:1000]
 y = y[:1000]
+
+from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
