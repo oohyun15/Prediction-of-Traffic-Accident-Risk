3009bc146d9293750bdfefd7684d4f561f2a865f
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-06-07 19:32:28 +0900

    model analysis complete

--- src/winprocess.py
+++ src/winprocess.py
@@ -31,17 +31,9 @@ data = pd.concat([data1,data2], axis = 1)
 x = data[['Time', 'Day', 'Location','RoadState','Weather', 'RoadShape', 'Type', 'AgeBand']]
 y = data['TargetOrigin']
 
-x = x[:100000]
-y = y[:100000]
+x = x[:1000]
+y = y[:1000]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-
-X, y = make_classification(n_features=8, n_informative=5,
-                            n_redundant=3, n_clusters_per_class=1, random_state=4, n_samples =1000 )
-
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.1)
 
 def execute_source(callback_imports, callback_name, callback_source, args):
     for callback_import in callback_imports:
